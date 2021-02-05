2b624fa630e9ed2c796272bab8c556b1d1a59328
ajcudd <ajcuddeback@gmail.com>
2021-02-05 11:21:48 -0500

    finished event api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const groupRoutes = require('./event-api-routes/group-routes');
+const eventRoutes = require('./event-api-routes/event-routes');
 
 router.use('/users', userRoutes);
 router.use('/groups', groupRoutes);
+router.use('/events', eventRoutes);
 
 module.exports = router;
