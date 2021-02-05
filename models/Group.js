d074833744377de95f8d807122a30092151f04e3
ajcudd <ajcuddeback@gmail.com>
2021-02-04 22:08:42 -0500

    added group table

---
+++ models/Group.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+
+class Group extends Model { }
+
+Group.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        group_title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        group_text: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'group'
+    }
+)
+
+module.exports = Group;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,6 @@
 const User = require('./User');
+const Group = require('./Group');
 
-module.exports = { User }
+
+
+module.exports = { User, Group }
