44710797fca2734f447c9e73e546bd7abfe510d9
ajcudd <ajcuddeback@gmail.com>
2021-02-05 12:10:02 -0500

    added handlebars to server

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const sequelize = require('./config/connection');
 const routes = require('./controllers');
 const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 const session = require('express-session');
 
 const app = express();
@@ -24,8 +26,13 @@ const sess = {
 
 app.use(session(sess));
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
