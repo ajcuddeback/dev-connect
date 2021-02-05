de7298280d1715ed302b45800cc1e8bfe68ab6b2
ajcudd <ajcuddeback@gmail.com>
2021-02-04 22:38:09 -0500

    added event to user associations

--- models/index.js
+++ models/index.js
@@ -4,6 +4,7 @@ const Event = require('./Event');
 const Group_Users = require('./Group_User');
 const Event_Users = require('./Event_User');
 
+// User to Group Associations
 User.hasMany(Group, {
     foreignKey: 'user_id'
 });
@@ -38,6 +39,44 @@ User.hasMany(Group_Users, {
 
 Group.hasMany(Group_Users, {
     foreignKey: 'group_id'
+});
+
+
+// User to Event associations
+User.hasMany(Event, {
+    foreignKey: 'user_id'
+});
+
+Event.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+User.belongsToMany(Event, {
+    through: Event_Users,
+    as: 'event_users',
+    foreignKey: 'user_id'
+});
+
+Event.belongsToMany(User, {
+    through: Event_Users,
+    as: 'event_users',
+    foreignKey: 'event_id'
+});
+
+Event_Users.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Event_Users.belongsTo(Event, {
+    foreignKey: 'event_id'
+});
+
+User.hasMany(Event_Users, {
+    foreignKey: 'user_id'
+});
+
+Event.hasMany(Event_Users, {
+    foreignKey: 'event_id'
 });
 
 
