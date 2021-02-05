9257925ef050a45234d11d4ad03f5a1741e587cd
ajcudd <ajcuddeback@gmail.com>
2021-02-04 21:54:43 -0500

    added session in server.js

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const sequelize = require('./config/connection');
+const routes = require('./controllers');
+const path = require('path');
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: process.env.SECRET,
+    cookie: {
+        expires: 30 * 1000
+    },
+    resave: true,
+    rolling: true,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
