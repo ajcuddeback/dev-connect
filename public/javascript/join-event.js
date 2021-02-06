aa0225372cd6d5d9d2670eea9bab2cd7145138f3
ajcudd <ajcuddeback@gmail.com>
2021-02-06 14:04:59 -0500

    added error handling for events

---
+++ public/javascript/join-event.js
@@ -0,0 +1,30 @@
+async function joinEventFormHandler(event) {
+    event.preventDefault();
+
+    const event_id = document.querySelector('.event_id').value;
+    const user_id = 1
+
+    const response = await fetch(`/api/events/add-user`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            event_id,
+            user_id
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.querySelector('.attend-event-succeess').classList.remove('hidden');
+        setTimeout(function () {
+            document.querySelector('.attend-event-succeess').classList.add('hidden');
+        }, 3000)
+    } else {
+        document.querySelector('.attend-event-fail').classList.remove('hidden');
+        setTimeout(function () {
+            document.querySelector('.attend-event-fail').classList.add('hidden');
+        }, 3000)
+    }
+}
+document.querySelector('.join-event-btn').addEventListener('click', joinEventFormHandler);
