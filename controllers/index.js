65769e735e4a85aedf8a0cdd986ed9c32621e1e6
Irina Skitayeva <irina.skitayeva@gmail.com>
2021-02-08 16:57:58 -0500

    created login/signup and logout handlebars, javascript, routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,15 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const homeRoutes = require("./home-routes.js");
 
-const apiRoutes = require('./api');
-const eventRouts = require('./events-routes/html-event-routes')
+const apiRoutes = require("./api");
+const eventRouts = require("./events-routes/html-event-routes");
 
-router.use('/api', apiRoutes);
-router.use('/meet', eventRouts)
+router.use("/api", apiRoutes);
+router.use("/meet", eventRouts);
+router.use("/", homeRoutes);
 
 router.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 
 module.exports = router;
