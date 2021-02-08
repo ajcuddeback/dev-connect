9b0d79452d4f2ebc65f61bb9b8c5e73c82feffce
ajcudd <ajcuddeback@gmail.com>
2021-02-08 18:21:50 -0500

    fixed issue

--- controllers/index.js
+++ controllers/index.js
@@ -1,20 +1,13 @@
 const router = require("express").Router();
 const homeRoutes = require("./home-routes.js");
 
-<<<<<<< HEAD
 const apiRoutes = require('./api');
 const eventRoutes = require('./events-routes/html-event-routes');
 
 router.use('/api', apiRoutes);
 router.use('/meet', eventRoutes);
-=======
-const apiRoutes = require("./api");
-const eventRouts = require("./events-routes/html-event-routes");
+router.use('/', homeRoutes);
 
-router.use("/api", apiRoutes);
-router.use("/meet", eventRouts);
-router.use("/", homeRoutes);
->>>>>>> 6dcff544418e5c3478b937606852e17f88c03f86
 
 router.use((req, res) => {
   res.status(404).end();
