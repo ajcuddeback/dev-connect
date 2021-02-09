c4f66f92430d053d2b59f5f9abdb53d240149872
Irina Skitayeva <irina.skitayeva@gmail.com>
2021-02-09 10:31:08 -0500

    fixed logout route

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -5,7 +5,7 @@ async function logoutButtonHandler() {
   });
 
   if (response.ok) {
-    document.location.replace("/");
+    document.location.replace("/login");
   } else {
     alert(response.statusText);
   }
