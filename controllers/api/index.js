32411279e341e185fae8a5aadb2a93727380ac22
Jephtebb <jephteborno@yahoo.com>
2021-02-11 18:52:08 -0500

    add social routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,9 +5,23 @@ const groupRoutes = require("./event-api-routes/group-routes");
 const eventRoutes = require("./event-api-routes/event-routes");
 const storeRoutes = require("./store-api-routes/");
 
+//declaring variables for social routes
+const socialUserRoutes = require('./social-user-routes');
+const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
+// end of social variables
+
 router.use("/users", userRoutes);
 router.use("/groups", groupRoutes);
 router.use("/events", eventRoutes);
 router.use("/store", storeRoutes);
+
+
+
+//add social routes to index
+router.use('/users', socialUserRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
+//end of social routes to index
 
 module.exports = router;
