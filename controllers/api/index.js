8aed5e502ffcfa569f20ec78abfae3e94cbebfe3
unknown <jasmineakbari@yahoo.com>
2021-02-11 20:19:21 -0500

    completed tag routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,11 +5,13 @@ const groupRoutes = require('./event-api-routes/group-routes');
 const eventRoutes = require('./event-api-routes/event-routes');
 const questionRoutes = require('./question-api-routes/question-routes');
 const answerRoutes = require('./question-api-routes/answer-routes');
+const tagRoutes = require('./question-api-routes/tag-routes');
 
 router.use('/users', userRoutes);
 router.use('/groups', groupRoutes);
 router.use('/events', eventRoutes);
 router.use('/askDevs', questionRoutes);
 router.use('/answers', answerRoutes);
+router.use('/tags', tagRoutes);
 
 module.exports = router;
