dc75e22de0dce761bb5b3971f98d8aa4bfa27e81
unknown <jasmineakbari@yahoo.com>
2021-02-11 20:08:04 -0500

    added route for answers in index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,10 +4,12 @@ const userRoutes = require('./user-routes');
 const groupRoutes = require('./event-api-routes/group-routes');
 const eventRoutes = require('./event-api-routes/event-routes');
 const questionRoutes = require('./question-api-routes/question-routes');
+const answerRoutes = require('./question-api-routes/answer-routes');
 
 router.use('/users', userRoutes);
 router.use('/groups', groupRoutes);
 router.use('/events', eventRoutes);
 router.use('/askDevs', questionRoutes);
+router.use('/answers', answerRoutes);
 
 module.exports = router;
